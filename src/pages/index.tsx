3677222bb7a891b5719f94f8e8886a2f3b3f0b66
Vinicius Sakai <viniciussakai0@gmail.com>
2021-01-23 12:56:06 -0300

    project: Create structure paths for nextJS

---
+++ src/pages/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Head from 'next/head'
+
+
+import { Container } from '../styles/pages/Home'
+
+const Home: React.FC = () => {
+  return (
+    <Container>
+      <Head>
+        <title>Homepage</title>
+      </Head>
+
+      <h1>ReactJS Structure</h1>
+      <p>A ReactJS + Next.js structure</p>
+    </Container>
+  )
+}
+
+export default Home
---
+++ src/styles/theme.ts
@@ -0,0 +1,9 @@
+const theme = {
+  colors: {
+    background: '#121214',
+    text: '#E1E1E6',
+    primary: '#1DD05E'
+  }
+}
+
+export default theme
